fb5cc82dcecdaafceb46efe3696bb834da6f6a03
ShinLungLee <103714759+ShinLungLee@users.noreply.github.com>
2022-11-03 14:25:05 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,10 +26,51 @@ $(document).scroll(function () {
         $('.asideTextArea').fadeOut();
     }
 });
+$(document).scroll(function () {
+    let y = $(this).scrollTop();
+    if (y > 200 ) {
+        $('.asideArtA').fadeIn();
+    } else {
+        $('.asideArtA').fadeOut();
+    }
 
-$(".asideArt").click(function(){
-    $(".asideArt").css('red');
+    if (y > 250) {
+        $('.asideTextAreaA').fadeIn();
+    } else {
+        $('.asideTextAreaA').fadeOut();
+    }
 });
+$(document).scroll(function () {
+    let y = $(this).scrollTop();
+    if (y > 200 ) {
+        $('.asideArtB').fadeIn();
+    } else {
+        $('.asideArtB').fadeOut();
+    }
+
+    if (y > 250) {
+        $('.asideTextAreaB').fadeIn();
+    } else {
+        $('.asideTextAreaB').fadeOut();
+    }
+});
+
+$(".asideArtA").mouseenter(function(){
+    $('.asideArtA').toggleClass('scaleUp');
+})
+$(".asideArtA").mouseout(function(){
+    $('.asideArtA').toggleClass('scaleDown');
+})
+$(".asideArtB").mouseenter(function(){
+    $('.asideArtB').toggleClass('scaleUp');
+})
+$(".asideArtB").mouseout(function(){
+    $('.asideArtB').toggleClass('scaleDown');
+})
+
+
+
+
 $("#signIn").click(function(){
     $("#form2").css('display','block');
     $("#form1").css('display','none');
